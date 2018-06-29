eaaeac3963fd3a4a4b6cd79a366f207ddf19c23f
Markiyan Hordiyenko <markhordi@gmail.com>
2018-06-29 16:10:33 +0000

    lesson practice

---
+++ js/index.js
@@ -0,0 +1,18 @@
+/*let msg ='Hello Worker!';
+alert(msg);
+let productTitle = 'Tesla X';
+let productPrice = 150000;
+let productAvailable = true;
+alert(productTitle);
+alert(productPrice);
+alert(productAvailable);*/
+
+const userName = prompt('Enter your name');
+const msg = 'Hello, ' + userName;
+alert(msg);
+
+const userAge = parseFloat(prompt('Enter your age'));
+const newAge = userAge + 1;
+const msgAge = 'Next year you will be ' + newAge;
+alert(msgAge);
+
